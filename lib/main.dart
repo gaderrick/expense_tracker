10e3b375adfc6f90023e3ad3a097509df05676e5
Jerry Derrick <jerry.derrick@gmail.com>
2023-06-25 10:01:29 -0500

    More Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -12,13 +12,13 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-    [
-      DeviceOrientation.portraitUp,
-    ],
-  ).then(
-    (fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //   [
+  //     DeviceOrientation.portraitUp,
+  //   ],
+  // ).then(
+  //   (fn) {
       runApp(
         MaterialApp(
           darkTheme: ThemeData.dark().copyWith(
@@ -62,6 +62,5 @@ void main() {
           home: const Expenses(),
         ),
       );
-    },
-  );
+    // });
 }
